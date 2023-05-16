f284dafc5ab0f34300c650430f84c5eb889bd451
David Rau <david@DavidsMacBook.local>
2023-05-16 21:43:16 +0200

    add git push

---
+++ utils.py
@@ -0,0 +1,22 @@
+from git import Repo
+
+PATH_OF_GIT_REPO = r'.'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'update'
+
+
+def git_push():
+    try:    
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.index.add(['server.py'])
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+        print('Updated arxiv...')
+    except:
+        print('Some error occured while pushing the code')    
+
+
+
+
+
+
